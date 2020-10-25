112c26b4831ccddacf473e8eefbac4bf2b62ed35
Bart Massey <bart.massey@gmail.com>
2020-10-25 14:29:24 -0700

    fixed bugs in mkdict.sh

--- mkdict.sh
+++ mkdict.sh
@@ -5,7 +5,10 @@ for d in freq.txt yawl.txt eowl.txt
 do
     gunzip <$d.gz >$DICT/$d
 done
+gunzip <freq-word.txt.gz >$DICT/freq.txt
 gunzip <scowl-80.txt.gz >$DICT/scowl.txt
+gunzip <yawl.txt.gz >$DICT/yawl.txt
+gunzip <eowl.txt.gz >$DICT/eowl.txt
 cat >$DICT/README-wordlists.txt <<'EOF'
 The dictionaries freq.txt, yawl.txt, eowl.txt and scowl.txt
 are from the wordlists project:
